115e0338d902e24f9152f1f19a4bee099d1e4691
gR <work.off.on@gmail.com>
2023-09-04 21:56:59 +0300

    init day 20

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by admin on 04.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+           
+    var body: some View {
+       Text("Hell")
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
