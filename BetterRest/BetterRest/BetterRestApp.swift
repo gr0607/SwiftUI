32a06a4efd5676aeac5a4392e7f07eee746ff602
gR <work.off.on@gmail.com>
2023-09-08 08:00:31 +0300

    init project 'day 26'

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by admin on 08.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by admin on 08.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello world")
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
