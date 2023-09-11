f25e18df1d72bc943ef1d0c545cdb07041958207
gR <work.off.on@gmail.com>
2023-09-11 22:15:45 +0300

    day 29 "init WorldScramble"

---
+++ WorldScrable/WorldScrable/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WorldScrable
+//
+//  Created by admin on 11.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+    }
+    
+ 
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WorldScrable/WorldScrable/WorldScrableApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrableApp.swift
+//  WorldScrable
+//
+//  Created by admin on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
