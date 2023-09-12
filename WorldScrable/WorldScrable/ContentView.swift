aff14d4ca4859e2398b80602cbedda85f8362258
gR <work.off.on@gmail.com>
2023-09-12 19:59:38 +0300

    day 30

--- WorldScrable/WorldScrable/ContentView.swift
+++ WorldScrable/WorldScrable/ContentView.swift
@@ -8,10 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     
     var body: some View {
-    }
+        NavigationView {
+            List {
+               
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }//Section
+                
+                Section {
+                    ForEach(usedWords, id:\.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                } //Section
+                
+            }//List
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        } //NavigationView
+    } //body
     
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
  
 }
 
