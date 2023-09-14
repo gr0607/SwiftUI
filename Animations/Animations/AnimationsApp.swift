409e725b20666f401b4a3b1cbe8626a7f0b914c0
gR <work.off.on@gmail.com>
2023-09-14 20:33:27 +0300

    day 32 "init animations"

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by admin on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
