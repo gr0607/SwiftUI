935110d5b5ff49800399374f4ca9a4e597d82bd6
gR <work.off.on@gmail.com>
2023-09-22 18:36:21 +0300

    day 40 begin

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by admin on 22.09.2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -11,10 +11,15 @@ import SwiftUI
 
 
 struct ContentView: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
-      Text("Hello world")
-            .padding()
+        VStack {
+            Text("\(astronauts.count)")
+                .padding()
+            Text("\(missions.count)")
+        }
          
     }//body
 }
